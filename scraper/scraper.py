f2d5223077160a3efbdefc2028b3a90bdb725c29
chaice <cb475@sussex.ac.uk>
2016-09-10 15:50:38 +0100

    pull every article from www.guardian.co.uk

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,22 +1,10 @@
 import urllib
 import re as rgx
 
-symbol_file = open('stock_names')
-symbol_list = symbol_file.readlines()
 
-symbols = list()
-for item in symbol_list:
-    item = item.split('\n')[0].lower()
-    symbols.append(item)
-
-print symbols
-
-for stock_symbol in symbols:
-    request = 'https://uk.finance.yahoo.com/q?s=' + stock_symbol + '&ql=1'
-    regex = '<span id="yfs_l84_' + stock_symbol + '">(.+?)</span>'
-    pattern = rgx.compile(regex)
-    htmlfile = urllib.urlopen(request)
-    htmltext = htmlfile.read()
-    price = rgx.findall(pattern, htmltext)
-    print stock_symbol + ':' + str(price)
-    print "----------------------------------------------------------------------------------------------------------"
+request = 'https://www.theguardian.com/uk'
+regex = '<a href="(.+?)" class="u-faux-block-link__overlay js-headline-text" data-link-name="article" tabindex="-1">'
+pattern = rgx.compile(regex)
+htmlfile = urllib.urlopen(request)
+htmltext = htmlfile.read()
+articles = rgx.findall(pattern, htmltext)
