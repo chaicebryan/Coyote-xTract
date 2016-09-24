c7deee3d3178da0277d1a17aa32a03862e154990
Chaice <cb475@sussex.ac.uk>
2016-09-24 20:12:39 +0100

    T2 - WIP  Added functionality for reddit crawling

--- scraper/ContentProcessor.py
+++ scraper/ContentProcessor.py
@@ -5,6 +5,7 @@ class ContentProcessor:
 
     def extract_content(self, page_url, hmtl_pattern):
         pattern = rgx.compile(hmtl_pattern)
+        print hmtl_pattern
         htmlfile = urllib.urlopen(page_url)
         htmltext = htmlfile.read()
         articles = rgx.findall(pattern, htmltext)
--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,9 +1,12 @@
 from ContentProcessor import ContentProcessor
 
 content_processor = ContentProcessor()
+
 guardian_articles = content_processor.extract_content('https://www.theguardian.com/uk', '<a href="(.+?)" class="u-faux-block-link__overlay js-headline-text" data-link-name="article" tabindex="-1">')
 print guardian_articles
 
+reddit_articles = content_processor.extract_content('https://www.reddit.com/r/news/', '<a class="title may-blank loggedin outbound " href="(.+?)"')
+print reddit_articles
 
 
 
