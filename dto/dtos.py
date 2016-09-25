bb67cee2cfd9e9a1d494313cf247ec503c98ae70
Chaice <cb475@sussex.ac.uk>
2016-09-25 03:02:13 +0100

    T1 - Add Article class

---
+++ dto/dtos.py
@@ -0,0 +1,11 @@
+class Article:
+
+    def __int__(self, id, author, title, description, text, creation_date, last_modified, category):
+        self.id = id
+        self.author = author
+        self.title = title
+        self.descrption = description
+        self.text = text
+        self.creation_date = creation_date
+        self.last_modified = last_modified
+        self.category = category
