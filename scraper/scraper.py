c690598b62ea6072462ad5cd646b0ff2d2bdf4e4
Chaice <cb475@sussex.ac.uk>
2016-09-27 22:04:00 +0100

    T6 - More suitable variable name

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -13,7 +13,7 @@ print bbc_articles
 
 ign_articles = content_processor.extract_content('http://uk.ign.com', 'href="http://www.ign.com/articles/(.+?)"')
 finishedIgnLinks = []
-for links in ign_articles:
+for article in ign_articles:
     newLink = 'http://www.ign.com/articles/' + links
     finishedIgnLinks.append(newLink)
 print finishedIgnLinks
