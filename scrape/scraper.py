d4d374dc802a562baa9ac135269377bc0c4fa187
Chaice <cb475@sussex.ac.uk>
2016-10-09 00:21:43 +0100

    Fixed a few errors and adding application.py as the application starting point

--- scrape/scraper.py
+++ scrape/scraper.py
@@ -3,11 +3,12 @@ from repository.connect import Connection
 
 class Scraper:
 
-    self.content_processor = ContentProcessor()
+    def __init__(self):
+        self.content_processor = ContentProcessor()
 
     def start(self):
         try:
-            self.conn = Connection('postgres', 'postgres', '1234', 'localhost', '5432')
+            self.conn = Connection('main', 'quavertrail', '1234', 'localhost', '5432')
             self.cursor = self.conn.begin()
         except:
             print 'Unable to connect to database'
@@ -20,11 +21,12 @@ class Scraper:
 
         for source in sources:
             self.content_processor.extract_content(source[1], source[2])
+        print 'Processing of sources complete'
 
 
     def load_sources(self):
         self.cursor.execute('SELECT * FROM sources;')
-        return self.cursor.fetch_all()
+        return self.cursor.fetchall()
 
 
 
